ec34f0b222f004528821b1cd1072e3d8d972841c
Vojtěch Oram <vojtech@leadspicker.com>
2021-04-03 22:07:20 +0200

    enhanced databazeknih spider (no prices)

--- scraper/spiders/databazeknih.py
+++ scraper/spiders/databazeknih.py
@@ -1,11 +1,13 @@
-import scrapy
+from bs4 import BeautifulSoup
+from matcher.utils import make_full_url
+from scraper.base_spider import BaseSpider
 
 
-class DatabazeknihSpider(scrapy.Spider):
+class DatabazeknihSpider(BaseSpider):
     name = 'databazeknih'
     allowed_domains = ['databazeknih.cz']
     start_urls = ['https://www.databazeknih.cz/knihy']
-    download_delay = 1
+    #download_delay = 1
 
     def parse(self, response):
         # parse genres
@@ -16,19 +18,48 @@ class DatabazeknihSpider(scrapy.Spider):
 
     def parse_list(self, response):
         for img_elem in response.css("img.inahled4"):
-            img = img_elem.attrib["src"]
-            if "/empty_n.jpg" in img:
-                print("Empty image!")
-                yield {}
-            else:
-                yield dict(
-                    cover=img,
-                    name=img_elem.attrib["title"],
-                    url=img_elem.xpath("parent::a").attrib["href"],
-                    author=img_elem.xpath("parent::a/following-sibling::span/text()").extract_first(),
-                )
+            url = make_full_url(response.url, img_elem.xpath("parent::a").attrib["href"])
+            yield response.request.replace(url=url, callback=self.parse_profile)
+
         # go to next page
         next_link = response.xpath('//span[@class="pnow"]/following-sibling::a')
         if next_link:
             url = next_link[0].attrib["href"]
             yield response.request.replace(url=url)
+
+    def parse_profile(self, response):
+        bs = BeautifulSoup(response.text, 'html.parser')
+        name = bs.select_one("h1[itemprop=name]").text
+        authors = [a.text for a in bs.select("span[itemprop=author] a")]
+        cover = bs.select_one(".kniha_img").get("src")
+        year = bs.select_one("span[itemprop=datePublished]")
+        if year:
+            year = year.text
+        meta = {
+            "item": dict(
+                name=name,
+                authors=authors,
+                cover=cover,
+                year=year,
+                profile_url=response.url,
+            )
+        }
+        book_id = response.url.split("-")[-1]
+        yield response.request.replace(
+            url=f"https://www.databazeknih.cz/books/book-detail-more-info-ajax.php?bid={book_id}",
+            callback=self.parse_more_info,
+            meta=meta,
+        )
+
+    def parse_more_info(self, response):
+        bs = BeautifulSoup(response.text, 'html.parser')
+        pages = bs.select_one("td[itemprop=numberOfPages]")
+        if pages:
+            pages = pages.text
+        isbn = bs.select_one("span[itemprop=isbn]")
+        if isbn:
+            isbn = isbn.text
+        item = response.meta["item"]
+        item["isbn"] = isbn
+        item["pages"] = pages
+        yield item
