30c8ecc0fbac41b3ec3009a36a724d3b7ea8be0b
Vojtech Oram <vojtech@oram.cz>
2021-04-16 20:16:11 +0200

    bookfinder: static root

--- bookfinder/settings.py
+++ bookfinder/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -109,6 +110,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static-collected")
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = "media/"
