c8f3be42761788598c1c8d7cb5d7287e5bef0575
Vojtech Oram <vojtech@oram.cz>
2021-06-08 14:12:53 +0200

    Added minimal download_delay

--- scraper/settings.py
+++ scraper/settings.py
@@ -33,7 +33,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 1
 #CONCURRENT_REQUESTS_PER_IP = 1
